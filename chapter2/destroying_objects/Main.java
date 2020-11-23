1157c7ba0747e51c18c6674156702b3b0b621494
Arnold Paye <arnold.paye@coxautoinc.com>
2020-11-23 07:15:03 -0400

    Add chapter3 directory

--- chapter2/destroying_objects/Main.java
+++ chapter2/destroying_objects/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+    public short bird = 32767;
+    public char mammal = (short)83;
+    // public char fish = (short)-1;
+    public long max = 3123456789L;
+    
+    public int octal = 017;
+    public int hexa = 0xff;
+    public byte bi = 0b110;
+
+    public float ff;
+    public double dd;
+
+    public float otherFloat = 10.0f;
+
+    public double notAtStart = 1_000.00;
     public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.bird);
+        System.out.println(main.mammal);
+        System.out.println(main.max);
+        System.out.println(main.octal);
+        System.out.println(main.hexa);
+        System.out.println(main.bi);
+
+        System.out.println(main.ff);
+        System.out.println(main.dd);
         System.gc();
     }
 }
---
+++ chapter3/java_operators/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        /*int cookies = 4;
+        double reward = 3 + 2 * --cookies;
+        System.out.println("Zoo animal receives: " + reward + " reward points");*/
+
+        /*boolean isAnimalAsleep = false;
+        System.out.println(isAnimalAsleep); // false
+        isAnimalAsleep = !isAnimalAsleep;
+        System.out.println(isAnimalAsleep); // true*/
+
+        double zooTemperature = 1.21;
+        System.out.println(zooTemperature); // 1.21
+        zooTemperature = -zooTemperature;
+        System.out.println(zooTemperature); // -1.21
+        zooTemperature = -(-zooTemperature);
+        System.out.println(zooTemperature); // -1.21
+    }
+}
