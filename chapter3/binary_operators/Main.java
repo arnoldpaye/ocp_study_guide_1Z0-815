0cba476f1c394f86e071098495ea225c963c2a3e
Arnold Paye <arnold.paye@coxautoinc.com>
2020-11-24 07:11:58 -0400

    Add unary/binary operators directory

---
+++ chapter3/binary_operators/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println(9 / 3); // 3
+        System.out.println(9 % 3); // 0
+
+        System.out.println(10 / 3); // 3
+        System.out.println(10 % 3); // 1
+
+        System.out.println(11 / 3); // 3
+        System.out.println(11 % 3); // 2
+
+        System.out.println(12 / 3); // 4
+        System.out.println(12 % 3); // 0*/
+
+        /*int x = 1;
+        long y = 33;
+        var z = x * y;
+        System.out.println(z); // long
+        System.out.println(((Object)z).getClass().getName());*/
+
+        /*double x = 39.21;
+        float y = 2.1f;
+        var z = x + y;
+        System.out.println(z); // double
+        System.out.println(((Object)z).getClass().getName());*/
+
+        /*short x = 10;
+        short y = 3;
+        var z = x * y;
+        System.out.println(z); // int
+        System.out.println(((Object)z).getClass().getName());*/
+
+        short w = 14;
+        float x = 13;
+        double y = 30;
+        var z = w * x / y;
+        System.out.println(z); // double
+        System.out.println(((Object)z).getClass().getName());
+    }
+}
---
+++ chapter3/unary_operators/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        /*int parkAttendance = 0;
+        System.out.println(parkAttendance); // 0
+        System.out.println(++parkAttendance); // 1
+        System.out.println(parkAttendance); // 1
+        System.out.println(parkAttendance--);// 1
+        Sytem.out.println(parkAttendance); // 0*/
+
+        /*int lion = 3;
+        int tiger = ++lion * 5 / lion--;
+        System.out.println("lion is " + lion);
+        System.out.println("tiger is " + tiger);*/
+
+        /*int price = 2 * 5 + 3 * 4 - 8;
+        System.out.println(price);*/
+
+        /*int price = 2 * ((5 + 3) * 4 - 8);
+        System.out.println(price);*/
+
+        // short robin = 3 + [(4 *2) + 4];
+    }
+}
