eb2c14182ffd97e815ba6d40ab9ff2ac47cec44e
Arnold Paye <arnold.paye@coxautoinc.com>
2020-12-21 08:03:03 -0400

    Add chapter5 directory.

---
+++ chapter5/creating_manipulating_strings/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println(1 + 2); // 3
+        System.out.println("a" + "b"); // ab
+        System.out.println("a" + "b" + 3); // ab3
+        System.out.println(1 + 2 + "c"); // 3c
+        System.out.println("c" + 1 + 2); // c12*/
+
+        /*String s = "1";
+        s += "2";
+        s += 3;
+        System.out.println(s);*/
+
+        /*String string = "animals";
+        System.out.println(string.length()); // 7*/
+
+        String string = "animals";
+        System.out.println(string.charAt(0)); // a
+        System.out.println(string.charAt(6)); // s
+        System.out.println(string.charAt(7)); // thows exception
+    }
+}
