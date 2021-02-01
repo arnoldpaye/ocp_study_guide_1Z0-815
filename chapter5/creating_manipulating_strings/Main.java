feb048002a1153099ec133714604100b540ca97c
Arnold Paye <arnold.paye@coxautoinc.com>
2021-02-01 08:00:29 -0400

    Add chapter5 files

--- chapter5/creating_manipulating_strings/Main.java
+++ chapter5/creating_manipulating_strings/Main.java
@@ -14,9 +14,34 @@ public class Main {
         /*String string = "animals";
         System.out.println(string.length()); // 7*/
 
-        String string = "animals";
+        /*String string = "animals";
         System.out.println(string.charAt(0)); // a
         System.out.println(string.charAt(6)); // s
-        System.out.println(string.charAt(7)); // thows exception
+        System.out.println(string.charAt(7)); // thows exception*/
+
+        /*String string = "animals";
+        System.out.println(string.indexOf('a')); // 0
+        System.out.println(string.indexOf("al")); // 4
+        System.out.println(string.indexOf('a', 4)); // 4
+        System.out.println(string.indexOf("al", 5)); // -1*/
+
+        /*String string = "animals";
+        System.out.println(string.substring(3)); // mals
+        System.out.println(string.substring(string.indexOf('m'))); // mals
+        System.out.println(string.substring(3, 4)); // m
+        System.out.println(string.substring(3, 7)); // mals*/
+
+        /*String string = "animals";
+        System.out.println(string.substring(3, 3)); // empty string
+        System.out.println(string.substring(3, 2)); // throw exception
+        System.out.println(string.substring(3, 8)); // throw exception*/
+
+        /*String string = "animals";
+        System.out.println(string.toUpperCase()); // ANIMALS
+        System.out.println("Abc123".toLowerCase()); // abc123*/
+
+        System.out.println("abc".equals("ABC")); // false
+        System.out.println("ABC".equals("ABC")); // true
+        System.out.println("abc".equalsIgnoreCase("ABC")); // true
     }
 }
